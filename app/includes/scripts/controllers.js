f14ef29f8dd893a8ff531bbd7c506998978913a7
Cameron Gallarno <cgallarno@gmail.com>
2013-06-22 13:22:26 -0400

    answers

--- app/includes/scripts/controllers.js
+++ app/includes/scripts/controllers.js
@@ -5,6 +5,7 @@ $app.controller('homeController', function ($scope, $rootScope, auth) {
 		auth.logout();
 		$scope.$navigate.go('/', 'none');
 	}
+
 	goToQuestions();
 
 	$scope.$on('authUpdated', function(){
@@ -22,16 +23,35 @@ $app.controller('homeController', function ($scope, $rootScope, auth) {
 	}
 });
 
-$app.controller('singleQuestion', function($scope, $routeParams, plus, auth){
-	var isNew = false;
-	if($routeParams.id == 'new') isNew = true;
+$app.controller('questionsController', function($scope, plus){
 
-	$scope.auth = auth.get();
+});
 
-	$scope.save = function(){
-		if(isNew){
-			$scope.question.time = new Date().getTime().toString();
-		}
-		plus.add('questions', $scope.question);
+$app.controller('newQuestion', function($scope, plus){
+	$scope.question = {};
+	$scope.save = function(){	
+		$scope.question.time = new Date().getTime().toString();
+		plus.add('questions', $scope.question).then(function(){
+			$scope.$navigate.go('/questions', 'slide', true);
+		});
+	}
+});
+
+$app.controller('answerQuestion', function($scope, $routeParams, plus){
+	console.log('question');
+
+	plus.get('questions', $routeParams.id).then(function(data){
+		console.log(data);
+		$scope.question = data;
+	});
+
+	$scope.answer = function(answer){
+		plus.add('answers', {
+			questionId : $routeParams.id,
+			time : new Date().getTime().toString(),
+			response : answer
+		}).then(function(){
+			$scope.$navigate.go('/questions', 'slide', true);
+		});
 	}
 });
