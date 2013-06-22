238b294be4f346a50ab866acda2bf1b6be6224af
Cameron Gallarno <cgallarno@gmail.com>
2013-06-22 11:47:14 -0400

    listing questions

--- app/includes/scripts/controllers.js
+++ app/includes/scripts/controllers.js
@@ -1,6 +1,10 @@
 'use strict';
 
-$app.controller('homeController', function ($scope, auth) {
+$app.controller('homeController', function ($scope, $rootScope, auth) {
+	$rootScope.logout = function(){
+		auth.logout();
+		$scope.$navigate.go('/', 'none');
+	}
 	goToQuestions();
 
 	$scope.$on('authUpdated', function(){
@@ -15,5 +19,19 @@ $app.controller('homeController', function ($scope, auth) {
 
 	$scope.login = function(){
 		auth.login();
+	}
+});
+
+$app.controller('singleQuestion', function($scope, $routeParams, plus, auth){
+	var isNew = false;
+	if($routeParams.id == 'new') isNew = true;
+
+	$scope.auth = auth.get();
+
+	$scope.save = function(){
+		if(isNew){
+			$scope.question.time = new Date().getTime().toString();
+		}
+		plus.add('questions', $scope.question);
 	}
 });
